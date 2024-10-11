76945dab4eaec6f7c5391dcb272fc45a88d920bb
DanielB566 <Danib7382@gmail.com>
2024-10-11 18:10:12 -0400

    added info button on hover to all images on showcase page now add a modal or smt when clicked

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
-$(document).ready(function(){
-    $("#infoBtn").delay(2000).show(0);
-    
-      // Show the popup and overlay on button click
-      $('#infoBtn').on('click', function () {
-        $('#overlay').fadeIn(300); 
-        $('#popup').fadeIn(300); 
-      });
+$(document).ready(function () {
+  $("#infoBtn").delay(2000).show(0);
 
-      // Hide the popup and overlay on close button click
-      $('#closeBtn').on('click', function () {
-        $('#overlay').fadeOut(300); 
-        $('#popup').fadeOut(300); 
-      });
+  // Show the popup and overlay on button click
+  $("#infoBtn").on("click", function () {
+    $("#overlay").fadeIn(300);
+    $("#popup").fadeIn(300);
+  });
+
+  // Hide the popup and overlay on close button click
+  $("#closeBtn").on("click", function () {
+    $("#overlay").fadeOut(300);
+    $("#popup").fadeOut(300);
+  });
+
+  $(document).on("keydown", function (event) {
+    if (event.key == "Escape") {
+      $("#overlay").fadeOut(300);
+      $("#popup").fadeOut(300);
+    }
+  });
+
+  // Hide the popup and overlay when clicking outside of the popup
+  $("#overlay").on("click", function () {
+    $(this).fadeOut(300);
+    $("#popup").fadeOut(300);
+  });
 
 
+  // Code for ShowCase Page starts here!
 
-      $(document).on('keydown', function(event) {
-        if (event.key == "Escape") {
-          $('#overlay').fadeOut(300); 
-          $('#popup').fadeOut(300); 
-        }
-    });
+  $(".gallery")
+  .on("mouseenter", function () {
+    $(this).find('#infoBtn2').show(); 
+  })
+  .on("mouseleave", function () {
+    $(this).find('#infoBtn2').hide(); 
+  });
 
-      // Hide the popup and overlay when clicking outside of the popup
-      $('#overlay').on('click', function () {
-        $(this).fadeOut(300); 
-        $('#popup').fadeOut(300); 
-      });
-    });
-  
-
+$('#infoBtn2').on('click', function() {
+  console.log("Button clicked");
+});
+});
